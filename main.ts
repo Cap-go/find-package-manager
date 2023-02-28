a50877aae4c3641b1e2e0c540ad362fdd18cc144
Martin DONADIEU <martindonadieu@gmail.com>
2023-02-28 18:59:11 +0800

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,15 @@
+export const findPackageManagerType = (path = '.') => {
+    const pnpmPath = `${path}/pnpm-lock.yaml`;
+    const yarnPath = `${path}/yarn.lock`;
+    const npmPath = `${path}/package-lock.json`;
+    if (existsSync(pnpmPath)) {
+        return 'pnpm'
+    }
+    if (existsSync(yarnPath)) {
+        return 'yarn'
+    }
+    if (existsSync(npmPath)) {
+        return 'npm'
+    }
+    return 'not-found'
+}
