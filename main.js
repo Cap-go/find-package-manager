6f9bb4a90d2108accb74c5e1b276460c2dd6ff17
Martin DONADIEU <martindonadieu@gmail.com>
2024-01-25 17:24:55 +0000

    Update main.js add bun

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const { existsSync } = require('fs');
 exports.findPackageManagerType = (path = '.') => {
+    const bunPath = `${path}/bun.lockb`;
     const pnpmPath = `${path}/pnpm-lock.yaml`;
     const yarnPath = `${path}/yarn.lock`;
     const npmPath = `${path}/package-lock.json`;
+    if (existsSync(bunPath)) {
+        return 'bun'
+    }
     if (existsSync(pnpmPath)) {
         return 'pnpm'
     }
